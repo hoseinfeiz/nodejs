f5978a5ef5c637b9e48bdc2bfc007f0dcbd232f1
hosein feizresan <hosein.feiz@gmail.com>
2024-01-24 13:37:10 +0330

    before add category filter

--- controllers/postController.js
+++ controllers/postController.js
@@ -2,13 +2,13 @@ const Category = require('../models/Category')
 const Post = require('../models/Post')
 
 const postController = async (req, res) => {
+  console.log(req.params.id)
   const catsArr = []
-  const post = await Post.findByPk(req.params.id)
+  const post = await Post.findByPk(Number(req.params.id))
   const cats = await Category.findAll()
   cats.forEach((cat) => {
     catsArr.push(cat.dataValues)
   })
-  console.log(post.dataValues)
 
   res.render('postpage', {
     post: post.dataValues,
