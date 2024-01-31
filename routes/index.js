1a81f5014341f70f18bfd3818523e783db4edec6
hosein feizresan <hosein.feiz@gmail.com>
2024-01-31 10:47:12 +0330

    before signin

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,16 @@ const express = require('express')
 const homepageController = require('../controllers/homepageController')
 const postController = require('../controllers/postController')
 const searchController = require('../controllers/searchController')
+const signinController = require('../controllers/signinController')
 
 const router = express.Router()
 
 router.get('/post/:id', postController)
 router.get('/page/:p_number', homepageController)
 router.get('/search', searchController)
+router.get('/login', signinController)
+router.post('/login', signinController)
+
 router.get('/', homepageController)
 
 module.exports = router
